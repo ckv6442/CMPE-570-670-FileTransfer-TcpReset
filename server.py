d41c8c3e770f09ab47af0f87755ac8b1c60e333a
James McDaid <James McDaid>
2023-12-06 15:30:20 -0500

    Added file transfer progress over time graph generation

--- server.py
+++ server.py
@@ -2,11 +2,13 @@ import socket
 import os
 import time
 from tqdm import tqdm
+import matplotlib.pyplot as plt
 
 def receive_file(connection, file_size):
     received_size = 0
     start_time = time.time()  # Start time
-
+    transfer_data = []  # List to store timestampled tuples (timestamp, received_size)
+     
     with tqdm(total=file_size, unit='B', unit_scale=True, desc="Receiving") as progress_bar:
         with open("ReceivedFile.txt", "wb") as file:
             while received_size < file_size:
@@ -15,11 +17,13 @@ def receive_file(connection, file_size):
                     break
                 file.write(data)
                 received_size += len(data)
-                progress_bar.update(len(data))
-
-    end_time = time.time()  # End time
-    duration = end_time - start_time  # Calculate duration
-    print(f"Time taken: {duration:.2f} seconds")
+                progress_bar.update(len(data))  # Update progress bar
+                transfer_data.append((time.time() - start_time, received_size))  # Record timestamp and data received
+            
+    end_time = time.time()
+    duration = end_time - start_time
+    print(f"Time taken: {duration:.2f} seconds") # Print total duration of file transfer     
+    return transfer_data # Returns transfer_data list to then visualize with matplotlib
     
 def start_server():
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -34,12 +38,21 @@ def start_server():
         file_size = int(client_socket.recv(1024).decode())
         print(f"File size: {file_size} bytes")
 
-        receive_file(client_socket, file_size)
+        transfer_data = receive_file(client_socket, file_size)
+        plot_transfer_progress(transfer_data)
 
         print("File received successfully")
-
         client_socket.close()
 
+def plot_transfer_progress(transfer_data):
+    timestamps, data_received = zip(*transfer_data)
+    data_received_MB = [bytes / (1024 * 1024) for bytes in data_received] # Convert data received from bytes to MB
+    plt.plot(timestamps, data_received_MB)
+    plt.xlabel('Time (seconds)')
+    plt.ylabel('Data Received (MB)')
+    plt.title('File Transfer Progress')
+    plt.show()
+    
 if __name__ == "__main__":
     host = '127.0.0.1'
     port = 12345
